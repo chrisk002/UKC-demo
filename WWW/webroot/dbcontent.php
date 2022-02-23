ac86223e343fbfd24aad39ada7fdb2fd3ee705d1
Chris Kinchin <chrisk002@hotmail.co.uk>
2022-02-23 20:06:23 +0000

    FW rule added. ro-user permissions updated to allow any host. web files updated to link to eachother. Issues with PHP query fixed

--- WWW/webroot/dbcontent.php
+++ WWW/webroot/dbcontent.php
@@ -3,15 +3,27 @@ $user="ro-user";
 $password="123456";
 $database="basic-web";
 $table="tblExampleData";
+$server="192.168.1.61";
 
-try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  echo "<h2>TODO</h2><ol>"; 
-  foreach($db->query("SELECT content FROM $table") as $row) {
-    echo "<li>" . $row['content'] . "</li>";
-  }
-  echo "</ol>";
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+$conn = mysqli_connect($server, $user, $password, $database);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
+
+$sql = "SELECT * FROM $table";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "<b>Record ID: </b>" . $row["recordID"]. " - <b>Record Content: </b>" . $row["recordText"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+
+echo "<a href='index.html'>Back</a>";
+?>
